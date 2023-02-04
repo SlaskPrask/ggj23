c2cd1b9994cb7ba2044959031089e48757021dbf
Matti Karjalainen <mkarjalainen@topcon.com>
2023-02-04 18:19:17 +0200

    Dialogue works stuff

---
+++ Assets/Scripts/DialogueTextInput.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DialogueTextInput : UIBehaviour
+{
+    private DialogueUI dialogueUI;
+    private TextField textField;
+
+    public override void InitUI(VisualElement element)
+    {
+        VisualElement button = element.Query("dialogue-submit");
+        button.AddManipulator(new Clickable(
+            e => { Submit(); }
+        ));
+
+        textField = element.Query<TextField>("dialogue-text-field");
+        textField.RegisterCallback<KeyDownEvent>(e =>
+        {
+            if (e.keyCode == KeyCode.Return)
+            {
+                Submit();
+            }
+        });
+    }
+
+    public void SetUp(DialogueUI dialogueUI)
+    {
+        this.dialogueUI = dialogueUI;
+    }
+
+    private void Submit()
+    {
+        dialogueUI.SubmitAnswer(textField.value);
+    }
+}
