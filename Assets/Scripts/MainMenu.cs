37ef4f9ef91a131b43d17c28161e18fd54e318d0
Matti Karjalainen <mkarjalainen@topcon.com>
2023-02-05 18:08:36 +0200

    main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -15,16 +16,19 @@ public class MainMenu : MonoBehaviour
         VisualElement start = uiDocument.rootVisualElement.Query("start-button");
         UIInputs.Button(start, () => { SceneManager.LoadScene(1); });
 
-        VisualElement quit = uiDocument.rootVisualElement.Query("start-button");
+        VisualElement quit = uiDocument.rootVisualElement.Query("quit-button");
         UIInputs.Button(quit, () => { Application.Quit(0); });
 
-        SetupSlider("volume-master");
+        SetupSlider("volume-master", () => AudioManager.masterVol, AudioManager.SetMaster);
+        SetupSlider("volume-music", () => AudioManager.musicVol, AudioManager.SetMusic);
+        SetupSlider("volume-sounds", () => AudioManager.sfxVol, AudioManager.SetSFX);
+        SetupSlider("volume-voice", () => AudioManager.voiceVol, AudioManager.SetVoice);
     }
 
     private void SetupSlider(string name, Func<float> getter, Action<float> setter)
     {
         Slider slider = uiDocument.rootVisualElement.Query<Slider>(name);
-        slider.value = getter();
-        slider.RegisterValueChangedCallback(e => { setter(e.newValue); });
+        slider.value = getter() * 100.0f;
+        slider.RegisterValueChangedCallback(e => { setter(e.newValue / 100.0f); });
     }
 }
